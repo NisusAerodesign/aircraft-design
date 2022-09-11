6a23ac0430803e62190956953a6868faf837cc42
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2022-09-11 18:06:17 -0300

    FUNCIONANDO!!

--- inputs.py
+++ inputs.py
@@ -1,3 +1,7 @@
+#%% Conversão
+kg2lb = 2.20462
+m2ft = 3.28084
+
 #%% Condições de Voo
 a0      = 340.3             #[m/s]
 g       = 9.80665           #[m/s^2]
@@ -11,21 +15,20 @@ a       = lambda H: (gamma*Rsp*T(H))**(0.5)
 
 #%% Aeronave
 Kld     = 15.5
-Sref    = 49            # [m²]
-LDmax   = 17             
-SFCloi  = 0.42/3600     # [kg/(kg.s)]
-SFCcru  = 0.5/3600      # [kg/(kg.s)]
-
-
+Sref    = 49*m2ft*m2ft      # [m²]
+SFCloi  = 0.42/3600         # [lb/(lbf.s)]
+SFCcru  = 0.642 /3600       # [lb/(lbf.s)]
+LDmax   = 16             
 
 #%% Missão
-R       = 6_500e3       # [m]
-Hcru    = 11e3          # [m]
+R       = 6_500e3 * m2ft       # [m]
+Hcru    = 11e3 *m2ft          # [m]
 Mcru    = 0.8        
-Vcru    = a(Hcru)*Mcru  # [m/s] 
-loiter  = 20*60         # [s] 
+Vcru    = a(Hcru)*Mcru *m2ft  # [m/s] 
+t_loiter = 20*60         # [s] 
 
-pmed    = 75         # kg
+pmed    = 75 *kg2lb         # kg
 Qt_p    = 10  
 crew    = 2
-payload = 1500       # kg
+payload = 1500 *kg2lb      # kg
+# %%
