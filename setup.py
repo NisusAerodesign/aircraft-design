3ed6b2033f0b4a8708353ca1399c83b28232cfd9
Irisson Lima <irisson2203@gmail.com>
2023-02-07 23:54:15 -0300

    Adição do download automático do binário AVL

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup, find_packages
 
-include_files = ['.foo']
+include_files = ['bin/.foo']
 
 with open('README.md', 'r') as arq:
     readme = arq.read()
 
 setup(name='aircraft-design',
-        version='0.0.5',
+        version='0.0.6',
         license='MIT',
         author='NisusAerodesign Irisson Lima',
         long_description=readme,
         long_description_content_type='text/markdown',
         author_email='ufsc.nisus@gmail.com',
-        keywords='aircraft design VLM vortex lattice',
+        keywords=['aircraft', 'design', 'VLM', 'vortex lattice'],
         description=u'Biblioteca para fazer análises de aeronaves',
         packages=find_packages(),
-        install_requires=['numpy', 'matplotlib', 'scipy', 'avlwrapper'],
-        package_data={
-        '': include_files
-        }
+        install_requires=['numpy', 'matplotlib', 'scipy', 'avlwrapper', 'requests'],
+        package_data={'': include_files},
 )     
