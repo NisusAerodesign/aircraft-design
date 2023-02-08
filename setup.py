d59d05bfde6dd40e0c635f18496edb8d909c4b56
Irisson Lima <irisson2203@gmail.com>
2023-02-08 00:08:23 -0300

    Adição da possibilidade de alterar o número de paineis

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as arq:
     readme = arq.read()
 
 setup(name='aircraft-design',
-        version='0.0.6',
+        version='0.0.7',
         license='MIT',
         author='NisusAerodesign Irisson Lima',
         long_description=readme,
